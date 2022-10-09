4ff7772e30aecd5a890a8361305984bf92cd0655
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2022-10-09 16:37:29 +0200

    Also publish for windows.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-arch="x86_64-unknown-linux-gnu"
+arch_linux="x86_64-unknown-linux-gnu"
+arch_windows="x86_64-pc-windows-gnu"
 
-cargo build --release --target $arch
-cp "./target/$arch/release/sensorhandler" "./sensorhandler-linux-x64"
+cargo build --release --target $arch_linux
+cargo build --release --target $arch_windows
+cp "./target/$arch_linux/release/sensorhandler" "./sensorhandler-linux-x64"
+cp "./target/$arch_windows/release/sensorhandler.exe" "./sensorhandler-windows-x64.exe"
